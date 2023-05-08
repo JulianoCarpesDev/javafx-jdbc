e224a31c11c39305b2870d23dacc268a691b192c
JulianoCarpes <julianocarpes.dev@gmail.com>
2023-05-08 08:59:30 -0300

    MainView Create

--- src/module-info.java
+++ src/module-info.java
@@ -1,5 +1,7 @@
 module javafxprojectjdbc {
 	requires javafx.controls;
+	requires javafx.fxml;
 	
 	opens application to javafx.graphics, javafx.fxml;
+	opens gui to javafx.graphics, javafx.fxml;
 }
