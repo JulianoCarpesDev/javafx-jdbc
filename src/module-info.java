edc343da5a8431c2e8b326ca7f8bedb0a74aa23b
JulianoCarpes <julianocarpes.dev@gmail.com>
2023-05-12 19:32:39 -0300

    Dialog design

--- src/module-info.java
+++ src/module-info.java
@@ -2,6 +2,8 @@ module javafxprojectjdbc {
 	requires javafx.controls;
 	requires javafx.fxml;
 	requires java.sql;
+	requires javafx.base;
+	requires javafx.graphics;
 	
 	opens application to javafx.graphics, javafx.fxml;
 	opens gui to javafx.graphics, javafx.fxml;
