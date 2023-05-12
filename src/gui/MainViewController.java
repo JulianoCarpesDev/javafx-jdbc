05fbdcce0559e677022e76652edcb1b956ded46e
JulianoCarpes <julianocarpes.dev@gmail.com>
2023-05-12 10:51:04 -0300

    database  conncetion  started

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -12,11 +12,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.VBox;
-import model.entites.Department;
 import model.services.DepartmentService;
 
 public class MainViewController implements Initializable{
--- src/module-info.java
+++ src/module-info.java
@@ -1,9 +1,10 @@
 module javafxprojectjdbc {
 	requires javafx.controls;
 	requires javafx.fxml;
+	requires java.sql;
 	
 	opens application to javafx.graphics, javafx.fxml;
 	opens gui to javafx.graphics, javafx.fxml;
-	opens model.entites to javafx.base;
+	opens model.entities to javafx.base;
 
 }
